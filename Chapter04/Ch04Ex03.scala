ebc8a28761a9392af4a826fde64a1bd494cf41bc
Nikita Duhovnijs <ndukhovny@gmail.com>
2018-04-05 21:56:38 +0300

    Ch04Ex03

--- Chapter04/Ch04Ex03.scala
+++ Chapter04/Ch04Ex03.scala
@@ -1,9 +1,9 @@
 val in = new java.util.Scanner(new java.io.File("Ch04Ex02input.txt"))
-val words = scala.collection.mutable.Map[String, Int]()
+var words = Map[String, Int]()
 
 while (in.hasNext()) {
   val next = in.next()
-  words(next) = words.getOrElse(next, 0) + 1
+  words += next -> (words.getOrElse(next, 0) + 1)
 }
 
 println(words.mkString("\n"))
