45da710ddd44c9d37db66379d17525932c3bdbe5
Nikita Duhovnijs <ndukhovny@gmail.com>
2018-04-22 17:06:13 +0300

    fix Ch05Ex03, Ch05Ex04

--- Chapter05/Ch05Ex04.scala
+++ Chapter05/Ch05Ex04.scala
@@ -1,6 +1,5 @@
 class Time() {
-  private var _hours = 0
-  private var _minutes = 0
+  private var _mins_since_midnight = 0
 
   def this(h: Int, m: Int) {
     this()
@@ -8,23 +7,38 @@ class Time() {
     minutes = m
   }
 
-  def before(other: Time) = (_hours * 60 + _minutes) < (other.hours * 60 + other.minutes)
+  def before(other: Time) = {
+    _mins_since_midnight < (other.hours * 60 + other.minutes)
+  }
 
-  def hours = _hours
-  def minutes = _minutes
+  def hours = _mins_since_midnight / 60 
+  def minutes = _mins_since_midnight % 60
 
   def hours_=(h: Int) {
-    if (h < 0 || h > 23) _hours = 0
-    else _hours = h
+    if (h >= 0 && h <= 23)  
+      _mins_since_midnight += h*60
+    else
+      _mins_since_midnight = _mins_since_midnight % 60
   }
 
   def minutes_=(m: Int) {
-    if (m < 0 || m > 59) _minutes = 0
-    else _minutes = m
+    if (m >= 0 && m <= 59) 
+      _mins_since_midnight += m
+    else
+      _mins_since_midnight = _mins_since_midnight / 60 * 60 
   }
 }
 
 var myTime = new Time(15, 30)
+println(f"Time is ${myTime.hours}%2d:${myTime.minutes}%2d")
+myTime.hours = -5
+println(f"Time is ${myTime.hours}%2d:${myTime.minutes}%2d")
+myTime.hours = 15
+println(f"Time is ${myTime.hours}%2d:${myTime.minutes}%2d")
+myTime.minutes = 66
+println(f"Time is ${myTime.hours}%2d:${myTime.minutes}%2d\n")
+
+myTime = new Time(15, 30)
 println(f"Time is ${myTime.hours}%2d:${myTime.minutes}%2d")
 println(f"Is it earlier than 16:30? ${myTime.before(new Time(16, 30))}!")
 println(f"Is it earlier than 14:30? ${myTime.before(new Time(14, 30))}!")
