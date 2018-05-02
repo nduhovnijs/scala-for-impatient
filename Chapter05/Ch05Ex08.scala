d055813a6bc8c5e09efe00736b416c81af75f50b
Nikita Duhovnijs <ndukhovny@gmail.com>
2018-05-02 21:28:53 +0300

    Ch05Ex08

--- Chapter05/Ch05Ex08.scala
+++ Chapter05/Ch05Ex08.scala
@@ -1,34 +1,26 @@
-// For primary constructor initially minimalist one is chosen.
-// It allows to reuse it in auxiliary constructurs.
-//
-// However, actually supplying 4 constructors is not necessary at all.
-// It's enough to have 1 with default values. 
-//
-// TODO: put all the tests, remake to single-primary constructor
-// Refresh knowledge of field usage in metod, pick toString (picked from other guys on GitHub)
-class Car(val manufacturer: String, val modelName: String) {
-  val modelYear = -1
-  var licensePlate = ""
-
-  def this(manufacturer: String, modelName: String, modelYear: Integer) {
-    this(manufacturer, modelName)
-    this.modelYear = _modelYear
+// Actually, if primary constructor is used with default parameters,
+// no auxiliary constructors are needed at all. 
+// Ok, maybe for convenience (for not specifying parameters explicitly), we can add one
+// auxiliary - for case when modelYear is not specified, but licensePlate is.
+class Car(val manufacturer: String, val modelName: String, val modelYear: Integer = -1, var licensePlate: String = "") {
+  def this(manufacturerArg: String, modelNameArg: String, licensePlateArg: String) {
+    this(manufacturerArg, modelNameArg, licensePlate=licensePlateArg)
   }
 
-  def this(manufacturer: String, modelName: String, licensePlate: String) {
-    this(manufacturer, modelName)
-    this.licensePlate = licensePlate
-  }
-
-  def this(manufacturer: String, modelName: String, modelYear: Integer, licensePlate: String) {
-    this(manufacturer, modelName)
-    this.modelYear = modelYear
-    this.licensePlate = licensePlate
-  }
+  // toString() was picked when comparing excercise solutions to some other solution on GitHub.
+  // Probably, it's properly addressed later in book. Will use now to print conveniently :) 
+  override def toString = s"${manufacturer} ${modelName} ${modelYear} ${licensePlate}"
 }
 
 val corolla = new Car("Toyota", "Corolla", 1992, "AB-1992")
-println(s"${corolla.manufacturer} ${corolla.modelName} ${corolla.modelYear} ${corolla.licensePlate}")
+//println(s"${corolla.manufacturer} ${corolla.modelName} ${corolla.modelYear} ${corolla.licensePlate}")
+println(corolla)
 
-val prius = new Car(modelName="Prius", manufacturer="Corolla")
-println(s"${prius.manufacturer} ${prius.modelName} ${prius.modelYear} ${prius.licensePlate}")
+val prius = new Car("Toyota", "Prius")
+println(prius)
+
+val aveo = new Car("Chevrolet", "Aveo", 2008)
+println(aveo)
+
+val insignia = new Car("Opel", "Insignia", "AC-2012")
+println(insignia)
