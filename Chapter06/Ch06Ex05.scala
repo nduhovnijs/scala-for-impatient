85365d27dbe7168c41a11eadf0194c03ec381193
Nikita Duhovnijs <ndukhovny@gmail.com>
2018-05-19 11:51:46 +0300

    Ch06Ex05

--- Chapter06/Ch06Ex05.scala
+++ Chapter06/Ch06Ex05.scala
@@ -1,4 +1,8 @@
 object Hello extends App {
+  /*
   val reverseArgs = for (i <- 0 to args.length - 1; j = args.length - 1 - i) yield args(j)
   println(reverseArgs.mkString(" "))
+  */
+  // f*ck, it appeard that there's "reverse" function for arrays
+  println(args.reverse.mkString(" "))
 }
