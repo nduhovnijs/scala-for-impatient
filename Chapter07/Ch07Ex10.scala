bde531a7f478c10ed3f0fb3eee4efd81383a7a8b
Nikita Duhovnijs <ndukhovny@gmail.com>
2018-06-14 07:23:32 +0300

    Ch07Ex10

---
+++ Chapter07/Ch07Ex10.scala
@@ -0,0 +1,14 @@
+// Looking at 
+//    https://docs.oracle.com/javase/8/docs/api/java/lang/package-summary.html
+// vs
+//    https://www.scala-lang.org/api/current/scala/index.html
+// ---
+//
+// Boolean
+// Byte
+//    Scala: Char, Java: Character
+// Double
+// Float
+//    Scala: Int, Java: Integer
+// Long 
+// StringBuilder
