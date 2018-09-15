7f56a815f8445f0c689e14cc52f79be849d9fcf4
Nikita Duhovnijs <ndukhovny@gmail.com>
2018-09-15 16:07:05 +0300

    Ch09Ex02

---
+++ Chapter09/Ch09Ex02.scala
@@ -0,0 +1,32 @@
+import scala.io.Source
+import java.io.PrintWriter
+import scala.collection.mutable.ArrayBuffer
+
+//TODO: refactor so that not to split line 2 times
+object Main extends App {
+  val source = Source.fromFile("sample.txt", "UTF-8")
+  val lines = for (line <- source.getLines.toArray) yield line.split("\t")
+  source.close()
+
+  val columnsWidths = new ArrayBuffer[Int]()
+
+  for (line <- lines) {
+    for (i <- 0 until line.length) {
+      if (i >= columnsWidths.length) {
+        columnsWidths += line(i).length 
+      } else {
+        if (columnsWidths(i) < line(i).length)
+          columnsWidths(i) = line(i).length
+      } 
+    }
+  }
+  
+  val out = new PrintWriter("sample_r.txt")
+  for (line <- lines) {
+    val replacedLine = for (i <- 0 until line.length) yield { 
+      line(i) + (" " * (columnsWidths(i) - (line(i).length)))
+    } 
+    out.println(replacedLine.mkString(" "))
+  }
+  out.close()
+}
