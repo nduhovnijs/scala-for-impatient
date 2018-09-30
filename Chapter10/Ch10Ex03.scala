99737d38b2ceb0558032c3ddb19aeb6d583001c3
Nikita Duhovnijs <ndukhovny@gmail.com>
2018-09-30 22:16:20 +0300

    Ch10Ex03

--- Chapter10/Ch10Ex03.scala
+++ Chapter10/Ch10Ex03.scala
@@ -1,1 +1,33 @@
-// For now skipping this excercise. Way too many manual work to draw class diagram.
+/*
+I'll shorten excercise to drawing diagram and making linearisation of the following scala.collection.TraversableLike
+
+Diagram:
+trait TraversableLike[]
+  trait HasNewBuilder[]
+    abstract class Any
+  trait FilterMonadic[]
+    abstract class Any
+  trait TraversableOnce[]
+    trait GenTraversableOnce[]
+      abstract class Any
+  trait GenTraversableLike
+    trait GenTraversableOnce[]
+      abstract class Any
+    trait Parallelizable[]
+      abstract class Any
+  trait Parallelizable[]
+    abstract class Any
+
+Order of construction
+1) Any
+2) HasNewBuilder
+3) FilterMonadic
+4) GenTraversableOnce
+5) TraversableOnce
+6) Parallelizable
+7) GenTraversableLike
+8) TraversableLike
+
+Linearization:
+lin(TraversableLike) = TraversableLike >> GenTraversableLike >> Parrallelizable >> TraversableOnce >> GenTraversableOnce >> FilterMonadic >> HasNewBuilder >> Any
+*/
