2f806b8629642f39940d24afadcfa68d6a23cbef
Nikita Duhovnijs <ndukhovny@gmail.com>
2018-11-08 08:00:34 +0200

    Ch11Ex02

--- Chapter11/Ch11Ex02.scala
+++ Chapter11/Ch11Ex02.scala
@@ -10,34 +10,35 @@ class MyNum(val myVal: Double) {
       myVal * **(a-1) 
   }
 
+  // to test problems with postfix operators
   def *+ = {
     "Test" 
   }
 }
 
 object Main extends App {
-  val num = new MyNum(2) 
+  val num = new MyNum(2)
+
   /*
     Could not use construction "**num(3)" since only +, -, ~, ! in Scala are working
     as a prefix unary operators converting to num._**(3).
   */
+  println(s"2 pow 3 = ${num **(3)}")
+  println(s"2 pow 0 = ${num **(0)}")
+  println(s"2 pow 1 = ${num **(1)}")
+  println(s"2 pow -3 = ${num **(-3)}")
 
   /*
-    Seems that pure postfix operators problems with parsing (requiring "-language:postfixOps"
-    therefore are related only to postfix operators without arguments - like
+    Seems that Scala problems related to postfix operators (requiring "-language:postfixOps"
+    therefore) are applicable only to postfix operators without arguments - 
     
   num *+
     
     so, this cannot be listed as a reason.
   */
 
-  println(s"2 pow 3 = ${num **(3)}")
-  println(s"2 pow 0 = ${num **(0)}")
-  println(s"2 pow 1 = ${num **(1)}")
-  println(s"2 pow -3 = ${num **(-3)}")
-
   /* 
-    Yet another problem - postfix operators have lower priority than infix operators.
+    More important problem - postfix operators have lower priority than infix operators.
     Which will make first two lines possible, and third one - not possible.
   */
   println(num **(3) * num.myVal)
@@ -47,7 +48,7 @@ object Main extends App {
 
   println(num *+)
 
-  /* Answer: 
+  /* Answer summary: 
  *    reason 1) - postfix operators have lower priority than infix.
  *    reason 2) - bad readability overall.
  */
