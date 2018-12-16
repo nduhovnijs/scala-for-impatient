cca0e909945e614f55aa4bbd1685e2ea74dcd38b
Nikita Duhovnijs <nikita@duhovnijs.me>
2018-12-16 13:58:59 +0200

    Ch11Ex10 (was skipped by mistake)

--- Chapter11/Ch11Ex10.scala
+++ Chapter11/Ch11Ex10.scala
@@ -1,32 +1,19 @@
-import scala.language.dynamics
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.io.File
 
-class DynamicProps(val props: java.util.Properties) extends Dynamic {
-  /*def updateDynamic(name: String)(value: String) {
-    props.setProperty(name.replaceAll("_", "."), value)
-  }*/
-
-  def selectDynamic(name: String): DynamicPropsHelper = {
-    new DynamicPropsHelper(s"${name}", props)
+object PathComponents {
+  def unapplySeq(path: Path): Option[Seq[String]]  = {
+    if (path.getNameCount() > 0) {
+      val pathSeq = for (i <- 0 to path.getNameCount()-1) yield path.getName(i).toString
+      Some(pathSeq)
+    } else
+      None
   }
-
-  /*def applyDynamicNamed(name: String)(args: (String, String)*) {
-    if (name != "add") throw new IllegalArgumentException
-    for((k, v) <- args)
-      props.setProperty(k.replaceAll("_", "."), v)
-  }*/
-}
-
-class DynamicPropsHelper(val name: String, val props: java.util.Properties) extends Dynamic {
-  def selectDynamic(name: String): DynamicPropsHelper = {
-    new DynamicPropsHelper(s"${this.name}.${name}", props)
-  }
-
-  override def toString = props.getProperty(this.name)
 }
 
 object Main extends App {
-  val sysProps = new DynamicProps(System.getProperties)
-
-  println(sysProps.java.vendor)
-  println(sysProps.java.vendor.url)
+  val path: Path = Paths.get("/home/nduhovnijs/readme.txt")
+  val PathComponents(first, second, third) = path
+  println(s"${first} ${second} ${third}")
 }
