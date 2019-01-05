a028697a94152415aec2e9544c4e35b6dde0b646
Nikita Duhovnijs <ndukhovny@gmail.com>
2019-01-05 12:28:35 +0200

    Ch12Ex03

--- Chapter12/Ch12Ex03.scala
+++ Chapter12/Ch12Ex03.scala
@@ -1,7 +1,14 @@
 object Main extends App {
-  def fac(arg: Int) = {
-    (1 to arg).reduceLeft(_ * _)
+  def fac(arg: Int): Option[Int] = {
+    if (arg < 0)
+      None
+    else if (arg == 0)
+      Some(1)
+    else
+      Some((1 to arg).reduceLeft(_ * _))
   }
 
+  println(fac(-1))
+  println(fac(0))
   println(fac(4))
 }
