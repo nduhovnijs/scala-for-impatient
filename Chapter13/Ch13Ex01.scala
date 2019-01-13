cf61fc0a9848832665e1f7f86f06adc215b4c0ed
Nikita Duhovnijs <nikita@duhovnijs.me>
2019-01-13 15:06:58 +0200

    Ch13Ex01

---
+++ Chapter13/Ch13Ex01.scala
@@ -0,0 +1,13 @@
+import scala.collection.mutable.Map
+import scala.collection.mutable.SortedSet
+
+object Main extends App {
+  // Using SortedSet to ensure order
+  def getIndexes(str: String): Map[Char, SortedSet[Int]] = {
+    // Task formulation itself prescribes to use mutable map.
+    str.indices.foldLeft(Map[Char, SortedSet[Int]]())((m, i) =>
+      { m += (str(i) -> (m.getOrElse(str(i), SortedSet[Int]()) + i)) })
+  }
+
+  println(getIndexes("Mississippi"))
+}
