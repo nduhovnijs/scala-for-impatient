3e6cc8afc2f15dd0208db1459c186e2b4d9863ec
Nikita Duhovnijs <nikita@duhovnijs.me>
2019-02-09 10:35:25 +0200

    Ch14Ex07

--- Chapter14/Ch14Ex06.scala
+++ Chapter14/Ch14Ex06.scala
@@ -1,9 +1,3 @@
-/*
-A better way of modeling such trees is with case classes. Let’s start with binary trees.
-Click here to view code image sealed
- Write a function to compute the sum of all elements in the leaves.
-*/
-
 abstract class BinaryTree
 case class Leaf(value: Int) extends BinaryTree
 case class Node(left: BinaryTree, right: BinaryTree) extends BinaryTree
@@ -14,6 +8,6 @@ object Main extends App {
     case Leaf(value) => value
   }
 
-  val l = Node(Leaf(3), Node(Node(Leaf(8), Leaf(2)), Leaf(5)))
+  val l = Node(Node(Leaf(3), Leaf(8)), Leaf(2), Node(Leaf(5)))
   println(elemSum(l))
 }
